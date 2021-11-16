aed9448c0cff82ac8f10c744f106270bfb2e050b
tarekyou <89763835+tarekyou@users.noreply.github.com>
2021-11-16 13:11:07 -0800

    built MVP

--- server.js
+++ server.js
@@ -48,6 +48,13 @@ personalityTraitsArray.forEach(trait => {
 }
 
 
+
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
+
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query){
@@ -55,6 +62,15 @@ app.get('/api/animals', (req, res) => {
     }
     res.json(results)
 })
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if(result){
+      res.json(result);
+    } else{
+        res.send(404)
+    } 
+  });
 
 
 // app.listen(3001, () => {
